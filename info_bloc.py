b190a2fe545d27e6a81ae1ce60ba62de4bea473e
ignaciosticco <ignaciosticco@gmail.com>
2016-06-03 19:12:58 -0300

    Ahora la funcion calcula el error (std) de presion y velocidad

--- info_bloc.py
+++ info_bloc.py
@@ -2,33 +2,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-data = np.genfromtxt('in.bloc_info_225p_v8_g0', delimiter = ' ')
+data = np.genfromtxt('in.bloc_info_225p_v4_g0', delimiter = ' ')
 
 
-speed = data[:,0]
-pressure = data[:,1]
+speed = data[:,2]
+pressure = data[:,3]
 
 def variables_promedio(data):
 	i=250*10  # num_indiv*cantidad de iter hasta 5000 (para sacar los primeros 5s)
-	p=0
-	N=0
-	v=0	
+	p = []
+	v = []	
 	while i<len(pressure):
 		if speed[i]!=0:
-			p+=pressure[i]
-			v+=speed[i]
-			N+=1
+			p+=[pressure[i]]
+			v+=[speed[i]]
 		i+=1
 
-	avg_v = v/N
+	avg_v = np.mean(v)
+	std_v = np.std(v)
 	avg_v = "%.2f" % avg_v
 	avg_v = float(avg_v)	
+	std_v = "%.2f" % std_v
+	std_v = float(std_v)	
 
-	avg_p = p/N		
+	
+	avg_p = np.mean(p)
+	std_p = np.std(p)
 	avg_p = "%.2f" % avg_p
 	avg_p = float(avg_p)
+	std_p = "%.2f" % std_p
+	std_p = float(std_p)
 
-	out = [avg_v,avg_p]
+	out = [avg_v,std_v,avg_p,std_p]
 	return out
 
 print(variables_promedio(data))
